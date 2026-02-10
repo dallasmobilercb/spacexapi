db9cb2a9ea73213ed79fb9d3287b6668535de82c
Plano Team <mobile@Planos-MacBook-Pro.local>
2026-02-10 11:43:05 -0600

    Initial repo for SpaceX API service call

---
+++ README.md
@@ -0,0 +1,19 @@
+Write a small app where you will consume the following endpoint:
+
+https://api.spacexdata.com/v3/launches
+
+Tools:
+
+	•	Feel free to use either MVVM or MVP
+	•	You can use any 3rd party library such as Retrofit, OkHttp, Dependency Injection Dagger/Hilt/Koin (Plus)
+	•	You can access to the internet to check for any documentation you might need (When needed).
+
+
+Feel free to build it with XML or Compose UI Design, the app will have the following requirements:
+
+	•	Home Screen
+	1.	Home Screen Title: SpaceX Rocket Launches
+	2.	Load a list with all Rockets received in the payload.
+	3.	The row item will have the following design: Launch Icon, Mission Name & Missiong Details
+
+Good Luck!
---
+++ app/build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    kotlin("kapt") version "2.2.21"
+}
+
+android {
+    namespace = "bofa.android.spacex"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "bofa.android.spacex"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    testImplementation(libs.junit)
+
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
+    implementation ("com.google.dagger:hilt-android:2.51.1")
+    kapt ("com.google.dagger:hilt-compiler:2.51.1")
+
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.3.2")
+}
